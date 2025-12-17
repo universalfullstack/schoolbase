a49eb3fb21ef718abbac24947d1a1e822601f8df
universalfullstack <97007475+universalfullstack@users.noreply.github.com>
2025-12-17 18:02:21 +0100

    Update plan.js

--- routes_base/plan.js
+++ routes_base/plan.js
@@ -8,11 +8,7 @@ import {
   deletePlan
 } from "../controllers/super_admin/planController.js";
 
-import { ensureRole } from "../middleware/auth.js";
-
 const router = express.Router();
-
-router.use(ensureRole("Super Admin"));
 
 router.get("/", listPlans);
 router.get("/create", showCreatePlan);
