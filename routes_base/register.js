0cbba483c68c7e9a0dccf3466afc72ddfa661b7e
universalfullstack <97007475+universalfullstack@users.noreply.github.com>
2025-12-17 12:01:13 +0100

    Update register.js

--- routes_base/register.js
+++ routes_base/register.js
@@ -13,8 +13,8 @@ const hashPassword = async (password) => {
 /* ===============================
    Render Registration Form
 ================================ */
-router.get("/register/:role", async (req, res) => {
-  const { role } = req.params;
+router.get("/register", async (req, res) => {
+  const { role } = req.body;
 
   const schools = await School.find().select("name");
 
