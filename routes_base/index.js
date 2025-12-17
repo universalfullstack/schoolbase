c441efec77233759ee560c93685b797d1c4e493d
universalfullstack <97007475+universalfullstack@users.noreply.github.com>
2025-12-17 11:51:57 +0100

    Update index.js

--- routes_base/index.js
+++ routes_base/index.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
 
 
 router.get('/login', (req, res) => {
-    res.render('login', {
+    res.render('auth/login', {
         layout: 'auth',
         title: 'Login'
     })
